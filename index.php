9ce5e5a33a111f0be7aaef05af67e9d9d709fb31
Рахматов Мирзобахтиёр Даврончонович <R.Mirzobakhtiyor@eskhata.tj>
2025-07-25 10:12:20 +0500

    Вёрстка главной стр. крипто сайта

---
+++ index.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <title>Вёрстка крипто сайта</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,7,9,10,chrome=1"> 
+    <meta name="keywords" content="вебсайт, сайт, создание вебсайта, лендинг, вебсайт на заказ, проект, портфолио, Мирзобахтиёр Рахматов, бот"/>
+    <meta name="description" content="Портфолио веб-разработчика Мирзобахтиёра: сайты, боты, SEO, фронтенд и бэкенд.">
+    <meta name="author" content="Мирзобахтиёр">
+    <meta property="og:title" content="Портфолио – Веб-разработчик Мирзобахтиёр">
+    <meta property="og:description" content="Создаю адаптивные сайты, SEO, ботов. HTML, CSS, JS, React, Node.js, PHP.">
+    <meta name="robots" content="index, follow">
+    <meta name="copyright" content="© 2025 Ehyo. Mirzobakhtiyor Rahmatov">
+    <meta name="og:title" content="Портфолио – Веб-разработчик. Создание сайтов">
+    <meta property="og:description" content="Портфолио веб-разработчика Мирзобахтиёра: сайты, боты, SEO, фронтенд и бэкенд."/>
+    <meta name="og:image" content="https://ehyo.tj/images/m-logo-400.png"/>
+	<meta property="og:image:alt" content="ehyo logo"/>
+	<meta property="og:site_name" content="Ehyo. Портфолио Рахматова Мирзобахтиёра" /> 
+	<meta property="og:type" content="page" />
+	<meta property="og:locale" content="ru_RU" />
+	<meta property="article:author" content="Mirzobakhtiyor Rahmatov" /> 
+	<meta property="article:publisher" content="Ehyo" />
+    
+    <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+    <link rel="stylesheet" href="css/index.css?v=<?= time(); ?>" />
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    
+</head>
+<body>
+    <section class="main">
+        <div class="content-wrap">
+            <div class="content-block">
+                <img src="./images/balloon.webp" class="balloon-top-right" alt="balloon"/>
+                <div class="content-container">
+                    <div class="notification-and-welcome-container">
+                        <div class="welcome-block">
+                            <div class="welcome-img-block">
+                                <img src="./images/icons/avatar.webp" alt="avatar icon" class="avatar-icon"/>
+                            </div>
+                            <div class="welcome-text-block">
+                                <h3 class="welcome-text-h3">Добро пожаловать</h3>
+                                <p class="author-name-p">Анар Гусейнов</p>
+                            </div>
+                        </div>
+                        
+                        <div class="notification-block">
+                            <img src="./images/icons/notification-dotbell.svg" alt="notification icon" class="notification-dot-icon"/>
+                        </div>
+                    </div>
+                    
+                    <div class="crypto-block">
+                        <figure>
+                            <div class="crypto-img-block">
+                                <div class="crypto-img-container">
+                                    <img src="./images/main-page/market.webp" alt="market" class="marker-img"/>
+                                </div>
+                            </div>
+                            <figcaption>Crypto</figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="crypto-img-block">
+                                <div class="crypto-img-container">
+                                    <img src="./images/main-page/phone-bitcoin.webp" alt="phone-bitcoin" class="phone-bitcoin-img"/>
+                                </div>
+                            </div>
+                            <figcaption>Crypto</figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="crypto-img-block">
+                                <div class="crypto-img-container">
+                                    <img src="./images/main-page/bitcoin.webp" alt="bitcoin" class="bitcoin-img"/>
+                                </div>
+                            </div>
+                            <figcaption>Crypto</figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="crypto-img-block">
+                                <div class="crypto-img-container">
+                                    <img src="./images/main-page/bitcoins-wallet.webp" alt="bitcoins-waller" class="bitcoins-wallet-img"/>
+                                </div>
+                            </div>
+                            <figcaption>Crypto</figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="crypto-img-block">
+                                <div class="crypto-img-container">
+                                    <img src="./images/main-page/four-coins.webp" alt="fourcoins" class="four-coins-img"/>
+                                </div>
+                            </div>
+                            <figcaption>Crypto</figcaption>
+                        </figure>
+                    </div>
+                    
+                    
+                    <div class="balance-block">
+                        <img src="./images/balloon.webp" class="balance-balloon-left" alt="balloon"/>
+                        <div class="balance-container">
+                            <div class="balance-banner">
+                                <p>
+                                    Общий баланс
+                                </p>
+                                <h3>
+                                    $16,500.80
+                                </h3>
+                            </div>
+                            <div class="investments-block">
+                                <div>
+                                    <p>Инвестировано</p>
+                                    <h3>$1,450,37</h3>
+                                </div>
+                                
+                                <div>
+                                    <p>Инвестировано</p>
+                                    <h3>$1,450,37</h3>
+                                </div>
+                                
+                                <div>
+                                    <p>Заработано</p>
+                                    <h3>$2,450,37</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="btc-eth-block">
+                        <img src="./images/balloon.webp" class="btc-eth-balloon-right" alt="balloon"/>
+                        <div class="btc-eth-container">
+                            <div class="btc-eth-elem">
+                                <div class="part1">
+                                    <div class="crypto-elem">
+                                        <div class="crypto-img-container">
+                                            <img src="./images/icons/crypto3.svg"/>
+                                        </div>
+                                        <p>ETH</p>
+                                    </div>
+                                    <div class="crypto-analysis">
+                                        <p>2,5 %</p>
+                                        <img src="./images/icons/linear-stroke-green.svg"/>
+                                    </div>
+                                </div>
+                                
+                                <div class="part2">
+                                    <h3>$12,250.00</h3>
+                                    <p>$1,120.28</p>
+                                </div>
+                            </div>
+                            
+                            <div class="btc-eth-elem">
+                                <div class="part1">
+                                    <div class="crypto-elem">
+                                        <div class="crypto-img-container crypto2-img">
+                                            <img src="./images/icons/crypto2.svg"/>
+                                        </div>
+                                        <p>BTC</p>
+                                    </div>
+                                    <div class="crypto-analysis">
+                                        <p>1,5 %</p>
+                                        <img src="./images/icons/linear-stroke-red.svg"/>
+                                    </div>
+                                </div>
+                                
+                                <div class="part2">
+                                    <h3>$14,130.00</h3>
+                                    <p>$2,310.00</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="transactions-block">
+                        <div class="transaction-container">
+                            <div class="transactions-elems">
+                                <div class="transaction-elem">
+                                    <img src="./images/icons/output.svg"/>
+                                    <p>Внести</p>
+                                </div>
+                                <div class="transaction-elem">
+                                    <img src="./images/icons/input.svg"/>
+                                    <p>Вывести</p>
+                                </div>
+                                <div class="transaction-elem">
+                                    <img src="./images/icons/input-output.svg"/>
+                                    <p>Обмен</p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+              
+                    <div class="transfers-block">
+                        <div class="transfer-balloon-container">
+                            <img src="./images/balloon.webp" class="transfer-balloon-bottom-center" alt="balloon"/>    
+                        </div>
+                        <div class="transfers-container">
+                            <h2>Переводы</h2>
+                            <div class="transfer-elems">
+                                <figure>
+                                    <div class="plus-container img-container">
+                                        <img src="./images/icons/plus.svg" alt="plus" class="plus-icon"/>
+                                    </div>
+                                </figure>
+                                
+                                <figure>
+                                    <div class="img-container">
+                                        <img src="./images/main-page/person1.webp" alt="person img" class="plus-icon"/>
+                                    </div>
+                                    <h3>Виталий</h3>
+                                    <p>Уровень 11</p>
+                                </figure>
+                                
+                                <figure>
+                                    <div class="img-container">
+                                        <img src="./images/main-page/person2.webp" alt="person img" class="plus-icon"/>
+                                    </div>
+                                    <h3>Виталий</h3>
+                                    <p>Уровень 11</p>
+                                </figure>
+                                
+                                <figure>
+                                    <div class="img-container">
+                                        <img src="./images/main-page/person3.webp" alt="person img" class="plus-icon"/>
+                                    </div>
+                                    <h3>Виталий</h3>
+                                    <p>Уровень 11</p>
+                                </figure>
+                                
+                                <figure>
+                                    <div class="img-container">
+                                        <img src="./images/main-page/person1.webp" alt="person img" class="plus-icon"/>
+                                    </div>
+                                    <h3>Виталий</h3>
+                                    <p>Уровень 11</p>
+                                </figure>
+                                
+                                <figure>
+                                    <div class="img-container">
+                                        <img src="./images/main-page/person2.webp" alt="person img" class="plus-icon"/>
+                                    </div>
+                                    <h3>Виталий</h3>
+                                    <p>Уровень 11</p>
+                                </figure>
+                                
+                                <figure>
+                                    <div class="img-container">
+                                        <img src="./images/main-page/person3.webp" alt="person img" class="plus-icon"/>
+                                    </div>
+                                    <h3>Виталий</h3>
+                                    <p>Уровень 11</p>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="statistics-block">
+                        <div class="statistics-container">
+                            <div class="statistics-control">
+                                <h3>Статистика</h3>
+                                <div class="month-container">
+                                    <p>Месяц</p>
+                                    <img src="./images/icons/arrow-bottom.svg" alt="arrow-bottom" class="arrot-bottom-img"/>
+                                </div>
+                            </div>
+                            <div class="statistics-chart">
+                                <img src="./images/main-page/chart.webp" alt="chart" class="chart-img"/>
+                            </div>
+                        </div>
+                    </div> 
+                    
+                    <nav>
+                        <div class="nav-container">
+                            <div class="nav-img-block-active">
+                                <img src="./images/icons/home-white.svg"/>
+                            </div>
+                            
+                            
+                            <div class="nav-img-block">
+                                <img src="./images/icons/alarm-gray.svg"/>
+                            </div>
+                            
+                            
+                            <div class="nav-p-block">
+                                <p>Банк</p>
+                            </div>
+                            
+                            
+                            <div class="nav-img-block">
+                                <img src="./images/icons/circulars-gray.svg"/>
+                            </div>
+                            
+                            <div class="nav-img-block">
+                                <img src="./images/icons/window-gray.svg"/>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>   
+</html>
